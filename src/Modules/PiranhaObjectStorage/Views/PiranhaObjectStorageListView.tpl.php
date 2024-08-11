79d33ebd525a7405e2776c6a4c166d28ec48f1ab
David Amanshia <dave@piranha.sh>
2024-08-11 03:07:29 +0100

    object storage list view

--- src/Modules/PiranhaObjectStorage/Views/PiranhaObjectStorageListView.tpl.php
+++ src/Modules/PiranhaObjectStorage/Views/PiranhaObjectStorageListView.tpl.php
@@ -1,56 +1,6 @@
 <?php
 
 
-function getListAsCLITable($list_entries) {
-    $outvar = '' ;
-    $max_lengths = [] ;
-    // get max lengths
-    foreach($list_entries as $list_entry) {
-        foreach($list_entry as $key=>$value) {
-            if (is_string($value)) {
-                if (isset($max_lengths[$key])) {
-                    $max_lengths[$key] = (strlen($value) > $max_lengths[$key]) ? strlen($value) : $max_lengths[$key] ;
-                    $max_lengths[$key] = $max_lengths[$key] + 1;
-                } else {
-                    $max_lengths[$key] = strlen($value) + 1;
-                }
-            } else {
-                $max_lengths[$key] = strlen($key) + 1;
-            }
-        }
-    }
-    // use title length if longer
-    foreach($max_lengths as $key=>$value) {
-        if (strlen($key) > $value) {
-            $max_lengths[$key] = strlen($key)+1 ;
-        }
-    }
-//    var_dump($max_lengths);
-    // get titles
-    foreach($max_lengths as $key=>$value) {
-        $outvar .= "$key".getSpaceString($value-strlen($key)) ;
-    }
-    $outvar .= "\n" ;
-    // get
-    foreach($list_entries as $list_entry) {
-        foreach($list_entry as $key=>$value) {
-            if (is_string($value)) {
-                $chars = $max_lengths[$key] - strlen($value) ;
-                $outvar .= "$value".getSpaceString($chars) ;
-            } else {
-                if (isset($max_lengths[$key])) {
-                    $chars = $max_lengths[$key] - strlen( "Array") ;
-                    $outvar .= "Array".getSpaceString($chars) ;
-                } else {
-                    $chars = $max_lengths[$key] - strlen( "NULL") ;
-                    $outvar .= "NULL".getSpaceString($chars) ;
-                }
-            }
-        }
-        $outvar .= "\n" ;
-    }
-    return $outvar ;
-}
 function getSpaceString($chars) {
     $string = '' ;
     for ($i=0; $i<$chars; $i++) {
@@ -65,7 +15,7 @@ if (isset($pageVars["piranhaResult"]['error'])) {
 
 } else {
 
-//    var_dump($pageVars);
+//    var_dump(array_keys($pageVars["piranhaResult"]['data']));
 
     $outVar = "" ;
     if(array_key_exists('buckets', $pageVars["piranhaResult"]['data'])) {
@@ -90,7 +40,31 @@ if (isset($pageVars["piranhaResult"]['error'])) {
 //            }
 
         }
-        $outVar = getListAsCLITable($pageVars["piranhaResult"]['data']['buckets']) ;
+        $outVar = \Model\PiranhaBaseLibs::getListAsCLITable($pageVars["piranhaResult"]['data']['buckets']) ;
+    }
+    if(
+        array_key_exists('bucket', $pageVars["piranhaResult"]['data']) &&
+        array_key_exists('objects', $pageVars["piranhaResult"]['data']) ) {
+        $object_count = count($pageVars["piranhaResult"]['data']['objects']) ;
+        for ($irow = 0; $irow < $object_count ; $irow++) {
+//        for ($irow = 0; $irow < 300 ; $irow++) {
+            unset($pageVars["piranhaResult"]['data']['objects'][$irow]['size_raw']);
+            unset($pageVars["piranhaResult"]['data']['objects'][$irow]['id']);
+            $pageVars["piranhaResult"]['data']['objects'][$irow]['last_modified'] = trim($pageVars["piranhaResult"]['data']['objects'][$irow]['last_modified']);
+//
+//            if (isset($pageVars["piranhaResult"]['data']['buckets'][$irow]['public'])) {
+//                $pageVars["piranhaResult"]['data']['buckets'][$irow]['public'] =
+//                    ($pageVars["piranhaResult"]['data']['buckets'][$irow]['public'] === true)
+//                        ? "true" : "false" ;
+//            }
+
+//            if (is_null($pageVars["piranhaResult"]['data']['objects'][$irow]['description'])) {
+//                $pageVars["piranhaResult"]['data']['objects'][$irow]['description'] = 'NULL' ;
+//            }
+
+        }
+//        $pageVars["piranhaResult"]['data']['objects'] = array_slice($pageVars["piranhaResult"]['data']['objects'], 100,200) ;
+        $outVar = \Model\PiranhaBaseLibs::getListAsCLITable($pageVars["piranhaResult"]['data']['objects']) ;
     }
     echo $outVar;
 
